13ab1a8ee46480e4e63f87b38583543ee8dfc711
RMS <madhu@rms.com>
2025-04-19 20:45:25 +0530

    Day2- program to print numbers 1-10 with “Pod <n> running” (e.g., “Pod 1 running”).

---
+++ Day2/print_pod_numbers.py
@@ -0,0 +1,11 @@
+def print_pod_numbers():
+    """ This function takes no input just print 10 limes of pod \n running"""
+    for i in range(1,11):
+        print(f"Pod {i} running")
+    
+
+def main():
+    print_pod_numbers()
+
+if __name__ == "__main__":
+    main()
